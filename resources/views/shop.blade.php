8f219d6cd7afdc793fbf917389c31456393093a1
Choon Teck <choonteck@Choons-MacBook-Air.local>
2025-04-21 02:21:43 +0800

    done filter

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -1,6 +1,14 @@
 @extends('layouts.app')
 @section('content')
 
+<style>
+
+  .category-list li{
+    line-height: 40px;
+  }
+
+</style>
+
 <main class="pt-90">
     <section class="shop-main container d-flex pt-4 pt-xl-5">
       <div class="shop-sidebar side-sticky bg-body" id="shopFilter">
@@ -10,10 +18,183 @@
         </div>
 
         <div class="pt-4 pt-lg-0"></div>
+        
+        <div class="accordion" id="brand-filters">
+          <div class="accordion-item mb-4 pb-3">
+            <h5 class="accordion-header" id="accordion-heading-brand">
+              <button class="accordion-button p-0 border-0 fs-5 text-uppercase" type="button" data-bs-toggle="collapse"
+                data-bs-target="#accordion-filter-brand" aria-expanded="true" aria-controls="accordion-filter-brand">
+                Brands
+                <svg class="accordion-button__icon type2" viewBox="0 0 10 6" xmlns="http://www.w3.org/2000/svg">
+                  <g aria-hidden="true" stroke="none" fill-rule="evenodd">
+                    <path
+                      d="M5.35668 0.159286C5.16235 -0.053094 4.83769 -0.0530941 4.64287 0.159286L0.147611 5.05963C-0.0492049 5.27473 -0.049205 5.62357 0.147611 5.83813C0.344427 6.05323 0.664108 6.05323 0.860924 5.83813L5 1.32706L9.13858 5.83867C9.33589 6.05378 9.65507 6.05378 9.85239 5.83867C10.0492 5.62357 10.0492 5.27473 9.85239 5.06018L5.35668 0.159286Z" />
+                  </g>
+                </svg>
+              </button>
+            </h5>
+            <div id="accordion-filter-brand" class="accordion-collapse collapse show border-0"
+              aria-labelledby="accordion-heading-brand" data-bs-parent="#brand-filters">
+              <div class="search-field multi-select accordion-body px-0 pb-0">
+                <div class="list list-inline mb-0 brand-list">
+                  <ul class="list list-inline mb-0 brand-list">
+                    @foreach ($brands as $brand)
+                      <li class="list-item">
+                        <span class="menu-link py-1">
+                          <input type="checkbox" name="brands" value="{{ $brand->id }}" class="chk-brand"
+                            @if(in_array($brand->id,explode(',', $f_brands))) checked="checked" @endif>
+                              {{$brand->name}}
+                        </span>
+                        <span class="text-right float-end">
+                          {{$brand->products->count()}}
+                        </span>
+                      </li>
+                    @endforeach
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+        <div class="accordion" id="categories-list">
+          <div class="accordion-item mb-4 pb-3">
+            <h5 class="accordion-header" id="accordion-heading-1">
+              <button class="accordion-button p-0 border-0 fs-5 text-uppercase" type="button" data-bs-toggle="collapse"
+                data-bs-target="#accordion-filter-1" aria-expanded="true" aria-controls="accordion-filter-1">
+                Product Categories
+                <svg class="accordion-button__icon type2" viewBox="0 0 10 6" xmlns="http://www.w3.org/2000/svg">
+                  <g aria-hidden="true" stroke="none" fill-rule="evenodd">
+                    <path
+                      d="M5.35668 0.159286C5.16235 -0.053094 4.83769 -0.0530941 4.64287 0.159286L0.147611 5.05963C-0.0492049 5.27473 -0.049205 5.62357 0.147611 5.83813C0.344427 6.05323 0.664108 6.05323 0.860924 5.83813L5 1.32706L9.13858 5.83867C9.33589 6.05378 9.65507 6.05378 9.85239 5.83867C10.0492 5.62357 10.0492 5.27473 9.85239 5.06018L5.35668 0.159286Z" />
+                  </g>
+                </svg>
+              </button>
+            </h5>
+            <div id="accordion-filter-1" class="accordion-collapse collapse show border-0"
+              aria-labelledby="accordion-heading-1" data-bs-parent="#categories-list">
+              <div class="accordion-body px-0 pb-0 pt-3">
+                <ul class="list list-inline mb-0">
+                  @foreach ($categories as $category)
+                  <li class="list-item">
+                    <span class="menu-link py-1">
+                      <input type="checkbox" name="categories" value="{{ $category->id }}" class="chk-category"
+                      @if(in_array($category->id,explode(',', $f_categories))) checked="checked" @endif >
+                      {{$category->name}}
+                    </span>
+                    <span class="text-right float-end">
+                      {{$category->products->count()}}
+                    </span>
+                  </li>
+                @endforeach
+                </ul>
+
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="accordion" id="color-filters">
+          <div class="accordion-item mb-4 pb-3">
+            <h5 class="accordion-header" id="accordion-heading-1">
+              <button class="accordion-button p-0 border-0 fs-5 text-uppercase" type="button" data-bs-toggle="collapse"
+                data-bs-target="#accordion-filter-2" aria-expanded="true" aria-controls="accordion-filter-2">
+                Color
+                <svg class="accordion-button__icon type2" viewBox="0 0 10 6" xmlns="http://www.w3.org/2000/svg">
+                  <g aria-hidden="true" stroke="none" fill-rule="evenodd">
+                    <path
+                      d="M5.35668 0.159286C5.16235 -0.053094 4.83769 -0.0530941 4.64287 0.159286L0.147611 5.05963C-0.0492049 5.27473 -0.049205 5.62357 0.147611 5.83813C0.344427 6.05323 0.664108 6.05323 0.860924 5.83813L5 1.32706L9.13858 5.83867C9.33589 6.05378 9.65507 6.05378 9.85239 5.83867C10.0492 5.62357 10.0492 5.27473 9.85239 5.06018L5.35668 0.159286Z" />
+                  </g>
+                </svg>
+              </button>
+            </h5>
+            <div id="accordion-filter-2" class="accordion-collapse collapse show border-0"
+              aria-labelledby="accordion-heading-1" data-bs-parent="#color-filters">
+              <div class="accordion-body px-0 pb-0">
+                <div class="d-flex flex-wrap">
+                  <a href="#" class="swatch-color js-filter" style="color: #0a2472"></a>
+                  <a href="#" class="swatch-color js-filter" style="color: #d7bb4f"></a>
+                  <a href="#" class="swatch-color js-filter" style="color: #282828"></a>
+                  <a href="#" class="swatch-color js-filter" style="color: #b1d6e8"></a>
+                  <a href="#" class="swatch-color js-filter" style="color: #9c7539"></a>
+                  <a href="#" class="swatch-color js-filter" style="color: #d29b48"></a>
+                  <a href="#" class="swatch-color js-filter" style="color: #e6ae95"></a>
+                  <a href="#" class="swatch-color js-filter" style="color: #d76b67"></a>
+                  <a href="#" class="swatch-color swatch_active js-filter" style="color: #bababa"></a>
+                  <a href="#" class="swatch-color js-filter" style="color: #bfdcc4"></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="accordion" id="size-filters">
+          <div class="accordion-item mb-4 pb-3">
+            <h5 class="accordion-header" id="accordion-heading-size">
+              <button class="accordion-button p-0 border-0 fs-5 text-uppercase" type="button" data-bs-toggle="collapse"
+                data-bs-target="#accordion-filter-size" aria-expanded="true" aria-controls="accordion-filter-size">
+                Sizes
+                <svg class="accordion-button__icon type2" viewBox="0 0 10 6" xmlns="http://www.w3.org/2000/svg">
+                  <g aria-hidden="true" stroke="none" fill-rule="evenodd">
+                    <path
+                      d="M5.35668 0.159286C5.16235 -0.053094 4.83769 -0.0530941 4.64287 0.159286L0.147611 5.05963C-0.0492049 5.27473 -0.049205 5.62357 0.147611 5.83813C0.344427 6.05323 0.664108 6.05323 0.860924 5.83813L5 1.32706L9.13858 5.83867C9.33589 6.05378 9.65507 6.05378 9.85239 5.83867C10.0492 5.62357 10.0492 5.27473 9.85239 5.06018L5.35668 0.159286Z" />
+                  </g>
+                </svg>
+              </button>
+            </h5>
+            <div id="accordion-filter-size" class="accordion-collapse collapse show border-0"
+              aria-labelledby="accordion-heading-size" data-bs-parent="#size-filters">
+              <div class="accordion-body px-0 pb-0">
+                <div class="d-flex flex-wrap">
+                  <a href="#" class="swatch-size btn btn-sm btn-outline-light mb-3 me-3 js-filter">XS</a>
+                  <a href="#" class="swatch-size btn btn-sm btn-outline-light mb-3 me-3 js-filter">S</a>
+                  <a href="#" class="swatch-size btn btn-sm btn-outline-light mb-3 me-3 js-filter">M</a>
+                  <a href="#" class="swatch-size btn btn-sm btn-outline-light mb-3 me-3 js-filter">L</a>
+                  <a href="#" class="swatch-size btn btn-sm btn-outline-light mb-3 me-3 js-filter">XL</a>
+                  <a href="#" class="swatch-size btn btn-sm btn-outline-light mb-3 me-3 js-filter">XXL</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="accordion" id="price-filters">
+          <div class="accordion-item mb-4">
+            <h5 class="accordion-header mb-2" id="accordion-heading-price">
+              <button class="accordion-button p-0 border-0 fs-5 text-uppercase" type="button" data-bs-toggle="collapse"
+                data-bs-target="#accordion-filter-price" aria-expanded="true" aria-controls="accordion-filter-price">
+                Price
+                <svg class="accordion-button__icon type2" viewBox="0 0 10 6" xmlns="http://www.w3.org/2000/svg">
+                  <g aria-hidden="true" stroke="none" fill-rule="evenodd">
+                    <path
+                      d="M5.35668 0.159286C5.16235 -0.053094 4.83769 -0.0530941 4.64287 0.159286L0.147611 5.05963C-0.0492049 5.27473 -0.049205 5.62357 0.147611 5.83813C0.344427 6.05323 0.664108 6.05323 0.860924 5.83813L5 1.32706L9.13858 5.83867C9.33589 6.05378 9.65507 6.05378 9.85239 5.83867C10.0492 5.62357 10.0492 5.27473 9.85239 5.06018L5.35668 0.159286Z" />
+                  </g>
+                </svg>
+              </button>
+            </h5>
+            <div id="accordion-filter-price" class="accordion-collapse collapse show border-0"
+              aria-labelledby="accordion-heading-price" data-bs-parent="#price-filters">
+              <input class="price-range-slider" type="text" name="price_range" value="" data-slider-min="10"
+                data-slider-max="1000" data-slider-step="5" data-slider-value="[250,450]" data-currency="$" />
+              <div class="price-range__info d-flex align-items-center mt-2">
+                <div class="me-auto">
+                  <span class="text-secondary">Min Price: </span>
+                  <span class="price-range__min">$250</span>
+                </div>
+                <div>
+                  <span class="text-secondary">Max Price: </span>
+                  <span class="price-range__max">$450</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+        
 
         {{-- Filter sections (categories, color, size, brand, price) remain unchanged --}}
       </div>
-
+      
       <div class="shop-list flex-grow-1">
         <div class="swiper-container js-swiper-slider slideshow slideshow_small slideshow_split" data-settings='{
             "autoplay": {
@@ -299,12 +480,17 @@
         </div>
       </div>
     </section>
+   
+
+
   </main>
 
   <form id="frmfilter" method="GET" action="{{ route('shop.index') }}">
     <input type="hidden" name="page" value="{{ $products->currentPage() }}">
     <input type="hidden" name="size" id="size" value="{{ $size }}">
     <input type="hidden" name="order" id="order" value="{{ $order }}">
+    <input type="hidden" name="brands" id="brands">
+    <input type="hidden" name="categories" id="hdncategories">
   </form>
 @endsection
 
@@ -315,9 +501,38 @@
       $("#size").val($("#pagesize option:selected").val());
       $("#frmfilter").submit();
     });
+
     $("#orderby").on("change", function(){
       $("#order").val($("#orderby option:selected").val());
       $("#frmfilter").submit();
+    });
+
+    $("input[name='brands']").on("change", function(){
+      var brands = "";
+      $("input[name='brands']:checked").each(function(){
+        if(brands == "") {
+          brands = $(this).val();
+        } else {
+          brands += "," + $(this).val();
+        }
+      });
+      $("#hdnbrands").val(brands);
+      $("#frmfilter").submit();
+
+    });
+
+    $("input[name='categories']").on("change", function(){
+      var categories = "";
+      $("input[name='categories']:checked").each(function(){
+        if(categories == "") {
+          categories = $(this).val();
+        } else {
+          categories += "," + $(this).val();
+        }
+      });
+      $("#hdncategories").val(categories);
+      $("#frmfilter").submit();
+
     });
   });
 </script>
